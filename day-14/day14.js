e6c0bf27ce9510a7e32518e7ac74f915ecf89352
vaibhav-rm <rathodvaibhav401@gmail.com>
2024-07-27 22:47:11 +0530

    updates

---
+++ day-14/day14.js
@@ -0,0 +1,12 @@
+// Task 1: Define a class Person with properties name and age, and a method to return a greeting message. Create an instance of the class and log the greeting message.
+class Person {
+    constructor(name, age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    greet() {
+        console.log("Hello...");
+    }
+
+}
